f63706ec6891e520687f539d4f80b8cd8742dcab
omarabdrbuo <omarkh3010@gmail.com>
2025-07-02 18:47:32 +0300

    first PHP push

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+<x-slot:heading>Contacts Page</x-slot:heading>
+
+contact page
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:heading>Home Page</x-slot:heading>
+    <!-- Page content here -->
+     home page
+</x-layout>
